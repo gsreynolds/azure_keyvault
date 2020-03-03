3abc116a2bbd93a6e60381d667c91dcf2b65abab
Gavin Reynolds <gavin@chef.io>
2020-03-03 19:09:09 +0000

    Add ability to use user-assigned managed identity and change akv_get_secret to use hash arguments
    Signed-off-by: Gavin Reynolds <gavin@chef.io>

--- libraries/key_vault.rb
+++ libraries/key_vault.rb
@@ -2,16 +2,24 @@
 
 module Azure
   module KeyVault
-    def akv_get_secret(vault, secret_name, spn = {}, secret_version = nil)
+    def akv_get_secret(options = {})
       require 'azure_key_vault'
 
+      vault = options.fetch(:vault, '')
+      secret_name = options.fetch(:secret, '')
+      spn = coerce_hash(options.fetch(:spn, {}))
+      user_assigned_msi = coerce_hash(options.fetch(:user_assigned_msi, {}))
+      secret_version = options.fetch(:secret_version, '')
+      secret_version = '' if secret_version.nil?
+
+      raise 'Vault name not provided' if vault.empty? || vault.nil?
+      raise 'Secret name not provided' if secret_name.empty? || secret_name.nil?
+
       vault_url = "https://#{vault}.vault.azure.net"
-      if secret_version.nil?
-        secret_version = ''
-      end
-      token_provider = create_token_credentials(spn)
+
+      token_provider = create_token_credentials(spn, user_assigned_msi)
       credentials = MsRest::TokenCredentials.new(token_provider)
-      client = KeyVaultClient.new(credentials)
+      client = Azure::KeyVault::V7_0::KeyVaultClient.new(credentials)
       response = client.get_secret(vault_url, secret_name, secret_version).value
       response
     end
@@ -25,12 +33,12 @@ module Azure
       end
     end
 
-    def create_token_credentials(spn)
+    def create_token_credentials(spn, user_assigned_msi)
       # We assume use MSI if spn is empty.
       # We define the port because we get a var deprecation error if not defined.
       if spn.nil? || spn.empty?
         @token_provider ||= begin
-          MsRestAzure::MSITokenProvider.new('50342', token_audience)
+          MsRestAzure::MSITokenProvider.new(50342, token_audience, user_assigned_msi)
         end
       else
         validate_service_principal!(spn)
@@ -48,6 +56,12 @@ module Azure
       spn['client_id'] ||= ENV['AZURE_CLIENT_ID']
       spn['secret'] ||= ENV['AZURE_CLIENT_SECRET']
       raise 'Invalid SPN info provided' unless spn['tenant_id'] && spn['client_id'] && spn['secret']
+    end
+
+    def coerce_hash(hash)
+      # Coerce the provided hash to a mash, and also remove any key named id in the case where a full data bag item has been passed
+      # msi_token_provider.rb in the ms_rest_azure gem will error if the msi hash is of length > 1
+      Mash.from_hash(hash.to_h).reject { |k, _v| k == 'id' }
     end
   end
 end
